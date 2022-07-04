c0b63f72485167dd69eaa4b29c8b3815e9cdf300
IsmayilMohsumov <mohsumovismayil@gmail.com>
2022-07-05 00:54:07 +0200

    Adding new test cases

--- src/test/java/com/example/junittesting/student/StudentServiceTest.java
+++ src/test/java/com/example/junittesting/student/StudentServiceTest.java
@@ -10,10 +10,12 @@ import org.mockito.MockitoAnnotations;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.never;
 
 class StudentServiceTest {
 
@@ -48,5 +50,48 @@ class StudentServiceTest {
         then(studentRepository).should().save(studentArgumentCaptor.capture());
         Student studentArgumentCaptorValue = studentArgumentCaptor.getValue();
         assertThat(studentArgumentCaptorValue).isEqualTo(student);
+    }
+
+    @Test
+    void itShouldNotSaveStudentWhenIndexNumberTaken() {
+        //Given
+        String indexNumber = "36467";
+        UUID id = UUID.randomUUID();
+
+        Student student = new Student(id, "Ismayil", "Mohsumov", indexNumber);
+
+        StudentRegistrationRequest request = new StudentRegistrationRequest(student);
+
+        //given() - force to not return value
+        given(studentRepository.selectStudentByIndexNumber(indexNumber)).willReturn(Optional.of(student));
+
+        //When
+        underTest.registerNewStudent(request);
+
+        //Then
+        then(studentRepository).should(never()).save(any());
+    }
+
+    @Test
+    void itShouldThrowAnExceptionWhenIndexNumberTaken() {
+        //Given
+        String indexNumber = "36467";
+        UUID id = UUID.randomUUID();
+
+        Student student = new Student(id, "Ismayil", "Mohsumov", indexNumber);
+        Student student2 = new Student(id, "Sue", "Mathilde", indexNumber);
+
+        StudentRegistrationRequest request = new StudentRegistrationRequest(student);
+
+        //given() - force to not return value
+        given(studentRepository.selectStudentByIndexNumber(indexNumber)).willReturn(Optional.of(student2));
+
+        //When
+        //Then
+        assertThatThrownBy(() -> underTest.registerNewStudent(request))
+                .isInstanceOf(IllegalStateException.class).hasMessageContaining(String.format("This index number [%s] is taken", indexNumber));
+
+        //Finally
+        then(studentRepository).should(never()).save(any(Student.class));
     }
 }
